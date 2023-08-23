f7f8e060866b143f624f47e3c862c46b68bd2c3b
osparking <jbpark03@gmail.com>
2023-08-23 22:07:18 +0900

    apply AServer default config'

--- src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,8 @@ public class SecurityConfig {
 	@Order(1)
 	SecurityFilterChain asSecurityFilterChain(HttpSecurity http)
 			throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		
 		return http.build();
 	}
 
