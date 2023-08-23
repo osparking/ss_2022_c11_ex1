0d74ed3fea91f7af9e9531e60a2f38da4fafd308
osparking <jbpark03@gmail.com>
2023-08-23 22:13:09 +0900

    choose to use default when further customication is possible

--- src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package space.bum.c11_ex1.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +17,8 @@ public class SecurityConfig {
 	SecurityFilterChain asSecurityFilterChain(HttpSecurity http)
 			throws Exception {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-		
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+				.oidc(Customizer.withDefaults());
 		return http.build();
 	}
 
