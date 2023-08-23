ba206341deed7b9e5d63f057404dd1956649e1e5
osparking <jbpark03@gmail.com>
2023-08-23 15:52:19 +0900

    authri' server initial state

---
+++ src/main/java/space/bum/c11_ex1/Ss2022C11Ex1Application.java
@@ -0,0 +1,13 @@
+package space.bum.c11_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C11Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss2022C11Ex1Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/c11_ex1/Ss2022C11Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.c11_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2022C11Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
