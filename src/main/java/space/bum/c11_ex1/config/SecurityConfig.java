39d8bcccab88351fb3186ba18dcc36f7e970873b
osparking <jbpark03@gmail.com>
2023-08-23 22:20:20 +0900

    access to unauthorize endpoint redirects to...

--- src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +20,13 @@ public class SecurityConfig {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
 				.oidc(Customizer.withDefaults());
+		
+		http.exceptionHandling(e ->
+				e.authenticationEntryPoint(
+						new LoginUrlAuthenticationEntryPoint("/login")
+						)
+				);
+		
 		return http.build();
 	}
 
