2c89dd4f1c602eeb67e308e665ac44189fef618e
osparking <jbpark03@gmail.com>
2023-08-24 10:35:28 +0900

    registered client repo bean

--- src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
+++ src/main/java/space/bum/c11_ex1/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package space.bum.c11_ex1.config;
 
 
+import java.util.UUID;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,9 +10,14 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -46,7 +53,6 @@ public class SecurityConfig {
 
 		return http.build();
 	}
-	// @formatter:on
 	
 	@Bean
 	UserDetailsService userDetailsService() {
@@ -62,5 +68,20 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
-	
+
+	@Bean
+	RegisteredClientRepository registeredClientRepository() {
+		var r1 = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("client").clientSecret("secret").scope(OidcScopes.OPENID)
+				.scope(OidcScopes.PROFILE)
+				.redirectUri("https://springone.io/authorized")
+				.clientAuthenticationMethod(
+						ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.build();
+
+		return new InMemoryRegisteredClientRepository(r1);
+	}
+	// @formatter:on
 }
